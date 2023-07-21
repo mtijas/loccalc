3f75f08acea8229367eb24aa9e7f48df303e46f0
Markus Ijäs <markus.ijas@outlook.com>
2023-07-21 22:04:13 +0300

    Implements first version of dynamic loading of plugins

--- loccalc.py
+++ loccalc.py
@@ -12,15 +12,27 @@ Total: 50
 
 '''
 
-import sys
+import sys, os
+from importlib import import_module
 from src import calculator
 from src.visualizers import totals_printer
 
 def main(argv):
     results = []
+    filetypes_to_counters = {}
+
+    items = os.listdir("src/counters")
+    for item in items:
+        if item.startswith("_") or os.path.isdir(item):
+            continue
+
+        item = item.removesuffix(".py")
+        instance = import_module(f"src.counters.{item}")
+        for filetype in instance.supported_filetypes:
+            filetypes_to_counters[filetype] = instance
 
     for arg in argv:
-        calculator.count_dir(arg, results)
+        calculator.count_dir(arg, results, filetypes_to_counters)
 
     totals_printer.visualize(results)
 
