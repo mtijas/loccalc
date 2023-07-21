30f8e9e5b12eebae7f251c638832d40ed17d64af
Markus Ijäs <markus.ijas@outlook.com>
2023-07-22 01:21:44 +0300

    Better multiline comment handling for python counter

--- src/counters/python.py
+++ src/counters/python.py
@@ -6,6 +6,7 @@ def count_lines(file):
     comments = 0
     empties = 0
     in_comment = False
+    starting_quotes = ""
 
     handle = open(file, 'r')
     for line in handle.readlines():
@@ -16,17 +17,33 @@ def count_lines(file):
         if not in_comment:
             if "" == stripped:
                 empties += 1
-            elif stripped.startswith(("'''", '"""')):
-                if "'''" not in stripped[3:] and '"""' not in stripped[3:]:
-                    in_comment = True
-                comments += 1
-            elif (stripped.startswith(("#"))):
+            elif stripped.startswith(("'''", '"""', "#")):
                 comments += 1
             else:
                 loc += 1
+
+            single_q_start = stripped.find("'''")
+            double_q_start = stripped.find('"""')
+
+            starting_quotes_position = -1
+
+            if -1 < single_q_start and -1 < double_q_start:
+                starting_quotes_position = min(single_q_start, double_q_start)
+                starting_quotes = '"""' if starting_quotes_position == double_q_start else "'''"
+            elif -1 < single_q_start:
+                starting_quotes_position = single_q_start
+                starting_quotes = "'''"
+            elif -1 < double_q_start:
+                starting_quotes_position = double_q_start
+                starting_quotes = '"""'
+
+            if (-1 < starting_quotes_position
+                    and starting_quotes not in stripped[starting_quotes_position+3:]):
+                in_comment = True
+
         else:
             comments += 1
-            if "'''" in stripped or '"""' in stripped:
+            if starting_quotes in stripped:
                 in_comment = False
 
     return {
