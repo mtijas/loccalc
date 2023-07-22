5b4a1291f3f9c244100cd77d898088263af4220f
Markus Ijäs <markus.ijas@outlook.com>
2023-07-22 14:59:06 +0300

    Adds multiline comment support for c

--- src/counters/c.py
+++ src/counters/c.py
@@ -1,10 +1,18 @@
+from ..utils import search
+
 supported_filetypes = ["c", "cpp", "h", "hpp"]
+
+comment_block_delimiters = {
+    "/*": "*/",
+}
 
 def count_lines(file):
     total = 0
     loc = 0
     comments = 0
     empties = 0
+    in_multiline_comment = False
+    comment_start_str = None
 
     handle = open(file, 'r')
     for line in handle.readlines():
@@ -12,12 +20,28 @@ def count_lines(file):
 
         stripped = line.strip()
 
-        if (stripped.startswith(("//", "/*", "*"))):
-            comments += 1
-        elif "" == stripped:
-            empties += 1
+        if not in_multiline_comment:
+            if (stripped.startswith(("//", "/*", "*"))):
+                comments += 1
+            elif "" == stripped:
+                empties += 1
+            else:
+                loc += 1
+
+            comment_start_str = search.find_multiline_starter(comment_block_delimiters, stripped)
+            if comment_start_str is not None:
+                in_multiline_comment = True
+
         else:
-            loc += 1
+            comments += 1
+            ending_position = stripped.find(comment_block_delimiters[comment_start_str])
+            if -1 < ending_position:
+                comment_start_str = search.find_multiline_starter(
+                    comment_block_delimiters,
+                    stripped[ending_position+3:]
+                )
+                if comment_start_str is None:
+                    in_multiline_comment = False
 
     return {
         "comments": comments,
