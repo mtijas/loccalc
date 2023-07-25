30a640c4cd87ee2ba3c829df8676857e38ba5afc
Markus Ijäs <markus.ijas@outlook.com>
2023-07-25 19:31:18 +0300

    Resolves #5

--- loccalc.py
+++ loccalc.py
@@ -13,32 +13,11 @@ Total: 83
 """
 
 import sys
-import os
-from importlib import import_module
 from src import calculator
-from src.visualizers import totals, by_filetype
 
 
 def main(argv):
-    results = []
-    filetypes_to_counters = {}
-
-    items = os.listdir("src/counters")
-    for item in items:
-        if os.path.isdir(item) or item.startswith("_") or not item.endswith(".py"):
-            continue
-
-        item = item.removesuffix(".py")
-        instance = import_module(f"src.counters.{item}")
-        if hasattr(instance, "supported_filetypes"):
-            for filetype in instance.supported_filetypes:
-                filetypes_to_counters[filetype] = instance
-
-    for arg in argv:
-        calculator.count_dir(arg, results, filetypes_to_counters)
-
-    by_filetype.visualize(results)
-    totals.visualize(results)
+    calculator.start(argv)
 
 
 if "__main__" == __name__:
--- src/calculator.py
+++ src/calculator.py
@@ -1,26 +1,61 @@
 import os
 from .utils import types
+from importlib import import_module
+from src.visualizers import totals, by_filetype
 
 
-def handle_file(file, filetypes_to_counters):
-    filetype = file.rsplit(".")[-1]
+def start(argv: list[str]):
+    filetypes_to_counters = load_counters()
+
+    for arg in argv:
+        results = count_dir(arg, filetypes_to_counters)
+
+    by_filetype.visualize(results)
+    totals.visualize(results)
+
+
+def load_counters() -> dict[str, object]:
+    filetypes_to_counters = {}
+
+    items = os.listdir("src/counters")
+    for item in items:
+        if os.path.isdir(item) or item.startswith("_") or not item.endswith(".py"):
+            continue
+
+        item = item.removesuffix(".py")
+        instance = import_module(f"src.counters.{item}")
+        if hasattr(instance, "supported_filetypes"):
+            for filetype in instance.supported_filetypes:
+                filetypes_to_counters[filetype] = instance
+
+    return filetypes_to_counters
+
+
+def handle_file(
+    file_name: str, filetypes_to_counters: dict[str, object]
+) -> types.LineCount:
+    filetype = file_name.rsplit(".")[-1]
     if filetype not in filetypes_to_counters.keys():
         return
 
     handler = filetypes_to_counters[filetype]
-    count_result = handler.count_lines(file)
+    count_result = handler.count_lines(file_name)
 
     return types.LineCount(**count_result)
 
 
-def count_dir(dir_path, results, filetypes_to_counters):
+def count_dir(
+    dir_path: str, filetypes_to_counters: dict[str, object]
+) -> list[types.ResultRow]:
+    results = list()
     if os.path.isdir(dir_path):
         dir_path = dir_path.rstrip("/")
         items = os.listdir(dir_path)
+
         for item in items:
             full_path = f"{dir_path}/{item}"
             if os.path.isdir(full_path):
-                count_dir(f"{full_path}/", results, filetypes_to_counters)
+                results.extend(count_dir(f"{full_path}/", filetypes_to_counters))
             else:
                 count_result = handle_file(full_path, filetypes_to_counters)
                 if count_result is not None:
@@ -32,6 +67,7 @@ def count_dir(dir_path, results, filetypes_to_counters):
                             linecount=count_result,
                         )
                     )
+
     elif os.path.isfile(dir_path):
         count_result = handle_file(dir_path, filetypes_to_counters)
         if count_result is not None:
@@ -43,3 +79,5 @@ def count_dir(dir_path, results, filetypes_to_counters):
                     linecount=count_result,
                 )
             )
+
+    return results
